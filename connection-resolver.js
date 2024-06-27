ffa2c4985264378d17d6bf3f4a220164c9a988cd
Swastika Pradhan <swastika.pradhan@laudio.com>
2024-06-27 14:15:39 +0545

    ci: github action to migrate

--- connection-resolver.js
+++ connection-resolver.js
@@ -2,27 +2,28 @@ exports.resolve = async (config) => {
     require('dotenv').config();
 
     const connections = [{
-        id: process.env.DB1_NAME,
+        id: process.env.DB_NAME,
         client: 'pg',
         connection: {
-            database: process.env.DB1_NAME,
-            user: process.env.DB1_USER,
-            host: process.env.DB1_HOST,
-            password: process.env.DB1_PASSWORD,
-            ssl: false
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            password: process.env.DB_PASSWORD,
+            ssl: process.env.ENV === 'local' ? false : { rejectUnauthorized: false }
         },
-    },
-    {
-        id: process.env.DB2_NAME,
-        client: 'pg',
-        connection: {
-            database: process.env.DB2_NAME,
-            user: process.env.DB2_USER,
-            host: process.env.DB2_HOST,
-            password: process.env.DB2_PASSWORD,
-            ssl: false
-        },
-    }]
+    }
+    // {
+    //     id: process.env.DB2_NAME,
+    //     client: 'pg',
+    //     connection: {
+    //         database: process.env.DB2_NAME,
+    //         user: process.env.DB_USER,
+    //         host: process.env.DB_HOST,
+    //         password: process.env.DB_PASSWORD,
+    //         ssl: false
+    //     },
+    // }
+]
 
     return connections;
 };
